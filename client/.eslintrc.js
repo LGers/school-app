e71c4fefa035cd334c718bd165a20663a3931354
Leonid Mychko <l.mychko@syberry.com>
2022-11-06 21:55:02 +0300

    489397: Add basic client setup

---
+++ client/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'spaced-comment': 'off',
+    "@typescript-eslint/no-unused-vars": ["warn"],
+    'react/jsx-props-no-spreading': 'off',
+    "jsx-a11y/label-has-associated-control": [2, {
+      "components": ["Input", "Input.Password", "Password"],
+      "controlComponents": ["Input", "Input.Password", "Password"],
+      "assert": "both",
+      "depth": 3,
+    }],
+    'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['state', 'axiosRequest'] }]
+  },
+};
---
+++ client/src/pages/Main/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Wrapper } from '../../components/Wrapper';
+
+export function Main() {
+  return (
+    <Wrapper>
+      <header><p>Header</p></header>
+      <main>
+        <h1>The Crawler - The school</h1>
+        <h2>Main Page</h2>
+      </main>
+      <footer>Footer</footer>
+    </Wrapper>
+  );
+}
