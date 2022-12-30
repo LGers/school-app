cf754603ad74b75e32bda5f8d281d91235f19905
Leonid Mychko <l.mychko@syberry.com>
2022-12-30 15:16:30 +0300

    540310: Add Main page

--- client/src/components/AuthHeader/AuthHeader.tsx
+++ client/src/components/AuthHeader/AuthHeader.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import s from './AuthHeader.module.scss';
 import logo from '../../assets/img/logo.png';
+import { AUTH_TITLE } from './AuthHeader.dictionary';
 
 export function AuthHeader() {
   return (
     <div className={s.authHeader}>
       <img className={s.authHeaderLogo} src={logo} alt="Logo" />
-      <div className={s.authHeaderTitle}>The school</div>
+      <div className={s.authHeaderTitle}>{AUTH_TITLE}</div>
     </div>
   );
 }
